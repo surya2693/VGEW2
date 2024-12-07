adc8dddd0e9caf2c70631c973ec77dc7ff204737
ramesh babu <rameshbabusoftware@gmail.com>
2024-12-07 20:47:01 +0530

    Material request Review

---
+++ lib/common_widgets/requestTextField.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class RequestTextField extends StatefulWidget {
+  final String? qtys;
+  final TextEditingController ctlr;
+  final String? hintname;
+
+   RequestTextField({ this.qtys,required this.ctlr,this.hintname});
+
+  @override
+  State<RequestTextField> createState() => _RequestTextFieldState();
+}
+
+class _RequestTextFieldState extends State<RequestTextField> {
+  @override
+  Widget build(BuildContext context) {
+    return
+    Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+
+          children: [
+            Text(
+              widget.qtys??'', // Label text
+              style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+            ),
+            Container(
+              height: 50,
+              child: TextField(
+                controller: widget.ctlr,
+                onChanged: (v) {},
+                decoration: InputDecoration(
+                  border: const OutlineInputBorder(),
+                  hintText: widget.hintname??'',
+
+                ),
+              ),
+            ),
+          ],
+        ),
+      );
+
+  }
+}
--- lib/ui/login/otpScreen.dart
+++ lib/ui/login/otpScreen.dart
@@ -3,8 +3,10 @@ import 'dart:developer';
 import 'package:base_flutter_provider_project/ui/login/splashScreen.dart';
 import 'package:flutter/material.dart';
 
+import '../../common_widgets/login_widgets/loginButton.dart';
 import '../../common_widgets/login_widgets/text_image.dart';
 import '../../common_widgets/login_widgets/textfield_buttonlogin.dart';
+import '../../packages/otpPinPut.dart';
 import '../../utils/commonTextFields.dart';
 import '../../utils/common_functions.dart';
 import '../dxf/dxfPage.dart';
@@ -12,7 +14,6 @@ import '../home/home_screen.dart';
 import '../mapScreen/mapScreen.dart';
 
 class OtpScreen extends StatefulWidget {
-
   @override
   State<OtpScreen> createState() => _OtpScreenState();
 }
@@ -24,55 +25,71 @@ class _OtpScreenState extends State<OtpScreen> {
 
   @override
   Widget build(BuildContext context) {
-    String otp='';
+    String otp = '';
     var devicewidth = MediaQuery.sizeOf(context).width;
 
     return Scaffold(
         backgroundColor: Colors.white,
-        body: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const SizedBox(height: 100),
-              Center(
-                child: TextFieldImage(
-                    title: 'Verification',
-                    subtitle: 'You will get a OTP via SMS',
-                    image: 'assets/images/loginotp.jpg'),
-              ),
-              const SizedBox(height: 50),
-              Center(
-                child: InkWell(
-                  onTap: (){
-                    Navigator.push(context, MaterialPageRoute(builder: (context)=>SplashScreen()));
-                  },
-                  child: TextFieldButton(
-                    datactlr: otpController,
-                    buttonname: 'VERIFY',
-                    label: 'OTP',
-                    onChange: (v){
-
-                      otp=v;
-                      setState(() {
-
-                      });
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(height: 100),
+                Center(
+                  child: TextFieldImage(
+                      title: 'Verification',
+                      subtitle: 'You will get a OTP via SMS',
+                      image: 'assets/images/loginotp.jpg'),
+                ),
+                const SizedBox(height: 50),
+                const Center(
+                  child: otpPinPut(),
+                ),
+                const SizedBox(height: 30),
+                Center(
+                  child: InkWell(
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => SplashScreen()));
                     },
+                    child: LoginButton(
+                      otpctlr: otpController,
+                      buttonname: 'VERIFY',
+                    ),
                   ),
                 ),
-              ),
-              const SizedBox(height: 5),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: InkWell(
-                  onTap: (){
-
-                  },
-                    child: const Text('Resend again',
-                        style: TextStyle(
-                            color: Colors.black, fontSize: 17,fontWeight: FontWeight.bold,))),
-              )
-            ],
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: InkWell(
+                      onTap: () {},
+                      child: const Center(
+                        child: Text('Didnt receive code?',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 17,
+                              fontWeight: FontWeight.normal,
+                            )),
+                      )),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: InkWell(
+                      onTap: () {},
+                      child: const Center(
+                        child: Text('Resend again',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 17,
+                              fontWeight: FontWeight.bold,
+                            )),
+                      )),
+                )
+              ],
+            ),
           ),
         ));
   }
